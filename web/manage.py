c0a4757fc2031a015d4b198ba889be69a2a4a3c5
victor <fengming19981221@163.com>
2019-11-04 23:10:44 +0800

    Django导入的异常,注释修改,翻译

--- web/manage.py
+++ web/manage.py
@@ -10,6 +10,8 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
+            "导入不了Django,你赶紧去看看你的python环境中拥护啥没有?"
+            "是不是你忘了激活你那个破虚拟环境了啊,SB"
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
